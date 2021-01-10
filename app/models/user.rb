6450c228a2a3da575f5c02fb94ee3cd0b909e562
Pascal Fodiman <pascal@fodiman.fr>
2021-01-10 18:47:55 +0100

    ajout d'une user show view très rustique

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-validates :last_name, :first_name, presence: true
+  validates :last_name, :first_name, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
