e583093755e0a8b1252727e4df1d4df89a24f8b7
alexis zaffalon <alexiszaffalon@live.fr>
2021-01-16 15:43:56 +0100

    solved update probelm

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]  #do
 #    resources :fishes, only: [:index]
 #  end
-  resources :fishes , only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+  resources :fishes, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :bookings, only: [:index, :new, :create, :edit, :update]
   end
 end
