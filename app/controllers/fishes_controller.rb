cce5448bac5f15a016e957fa2c7db3696c370e27
alexis zaffalon <alexiszaffalon@live.fr>
2021-01-16 14:12:21 +0100

    solved conflicts

--- app/controllers/fishes_controller.rb
+++ app/controllers/fishes_controller.rb
@@ -53,10 +53,6 @@ class FishesController < ApplicationController
   end
 
   def fish_params
-<<<<<<< HEAD
-    params.require(:fish).permit(:name, :price_per_week, :colour, :length, :fish_type_id, :photo)
-=======
-    params.require(:fish).permit(:name, :price_per_week, :colour, :length, :fish_type_id, :available)
->>>>>>> master
+    params.require(:fish).permit(:name, :price_per_week, :colour, :length, :fish_type_id, :available, :photo)
   end
 end
