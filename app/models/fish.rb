82bd771fcaae44f443f2a0eb415da1dfd29113b5
Pascal Fodiman <pascal@fodiman.fr>
2021-01-19 11:38:37 +0100

    add geocoding and maps - fixed navbar

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -3,7 +3,8 @@ class Fish < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo, dependent: :destroy
-
+  geocoded_by :address
   validates :name, presence: true
+  after_validation :geocode, if: :will_save_change_to_address?
   # validates :price_per_week, numericality: { only_integer: true }
 end
