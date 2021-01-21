9c31583f0c3abd09034a1424a415171becefe293
Pascal Fodiman <pascal@fodiman.fr>
2021-01-21 13:09:23 +0100

    add date validation to Booking. To be completed further

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,11 +1,17 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :fish
+  validate :end_at_after_start_at
+  validates :start_at, :end_at, presence: true, inclusion: { in: (Date.today..Date.today+1.years) }
 
-  def current_status_range
-    current_status_range = []
-    return current_status_range if current_status == 'En attente'
 
-    current_status_range = ['Validée', 'Refusée']
+  private
+
+  def end_at_after_start_at
+    return if end_at.blank? || start_at.blank?
+
+    if end_at < start_at
+      errors.add :end_at, "Doit être postérieure à la date de début de réservation"
+    end
   end
 end
